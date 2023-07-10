f477152ec65b6600a1fb0da7bbc39bada1f33bc1
Simisola481 <simisola_1@tamu.edu>
2023-07-10 10:56:31 -0500

    style changes & updates

--- index.js
+++ index.js
@@ -13,4 +13,39 @@ navLinks.forEach(link => {
 });
 
 
+    // JavaScript code for horizontal scrolling
+    const cardSection = document.querySelector('.card-section');
+    const cards = document.querySelectorAll('.card_port');
+    const scrollSpeed = 2;
+
+    let scrollPos = 0;
+
+    function scrollRight() {
+      scrollPos += scrollSpeed;
+      cardSection.scrollTo(scrollPos, 0);
+      if (scrollPos < cardSection.scrollWidth - cardSection.clientWidth) {
+        requestAnimationFrame(scrollRight);
+      }
+    }
+
+    function scrollLeft() {
+      scrollPos -= scrollSpeed;
+      cardSection.scrollTo(scrollPos, 0);
+      if (scrollPos > 0) {
+        requestAnimationFrame(scrollLeft);
+      }
+    }
+
+    cardSection.addEventListener('mouseenter', () => {
+      cancelAnimationFrame(scrollRight);
+      cancelAnimationFrame(scrollLeft);
+    });
+
+    cardSection.addEventListener('mouseleave', () => {
+      if (scrollPos < cardSection.scrollWidth - cardSection.clientWidth) {
+        requestAnimationFrame(scrollRight);
+      }
+    });
+
+    scrollRight();
 
